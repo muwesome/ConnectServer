a620378b3c856add7fddf98ee3158c26f84754ef
Elliott Linder <elliott.darfink@gmail.com>
2018-10-31 11:38:26 +0100

    fix: remove obsolete mutex usage

--- src/state/realm.rs
+++ src/state/realm.rs
@@ -1,7 +1,6 @@
 use chashmap::CHashMap;
 use crate::util::Dispatcher;
 use failure::Fail;
-use parking_lot::Mutex;
 use std::{cell::RefCell, ops::DerefMut};
 use std::{fmt, sync::Arc};
 
@@ -49,50 +48,40 @@ pub enum RealmBrowserError {
   InexistentId,
 }
 
-struct RealmBrowserInner {
-  dispatcher: Dispatcher<RealmListener>,
-}
-
 #[derive(Clone)]
 pub struct RealmBrowser {
-  inner: Arc<Mutex<RealmBrowserInner>>,
-  map: Arc<CHashMap<RealmServerId, RealmServer>>,
+  dispatcher: Dispatcher<RealmListener>,
+  realms: Arc<CHashMap<RealmServerId, RealmServer>>,
 }
 
 impl RealmBrowser {
   pub fn new() -> Self {
     RealmBrowser {
-      inner: Arc::new(Mutex::new(RealmBrowserInner {
-        dispatcher: Dispatcher::new(),
-      })),
-      map: Arc::new(CHashMap::new()),
+      dispatcher: Dispatcher::new(),
+      realms: Arc::new(CHashMap::new()),
     }
   }
 
   pub fn subscribe(&self, listener: &Arc<RealmListener>) {
-    let inner = self.inner.lock();
-    inner.dispatcher.subscribe(listener);
+    self.dispatcher.subscribe(listener);
   }
 
   pub fn add(&self, realm: RealmServer) -> Result<(), RealmBrowserError> {
-    if self.map.contains_key(&realm.id) {
+    if self.realms.contains_key(&realm.id) {
       Err(RealmBrowserError::DuplicateId)?;
     }
 
-    let inner = self.inner.lock();
-    inner.dispatcher.dispatch(|l| l.on_register(&realm));
-    self.map.insert_new(realm.id, realm);
+    self.dispatcher.dispatch(|l| l.on_register(&realm));
+    self.realms.insert_new(realm.id, realm);
     Ok(())
   }
 
   pub fn remove(&self, id: RealmServerId) -> Result<(), RealmBrowserError> {
-    let inner = self.inner.lock();
     self
-      .map
-      .get(&id)
-      .map(|realm| inner.dispatcher.dispatch(|l| l.on_deregister(&realm)))
+      .realms
+      .remove(&id)
+      .map(|realm| self.dispatcher.dispatch(|l| l.on_deregister(&realm)))
       .ok_or(RealmBrowserError::InexistentId)?;
-    self.map.remove(&id);
     Ok(())
   }
 
@@ -100,19 +89,18 @@ impl RealmBrowser {
   where
     F: FnOnce(&mut RealmServer),
   {
-    let inner = self.inner.lock();
     self
-      .map
+      .realms
       .get_mut(&id)
       .map(|mut realm| {
         mutator(&mut realm);
-        inner.dispatcher.dispatch(|l| l.on_update(&realm));
+        self.dispatcher.dispatch(|l| l.on_update(&realm));
       }).ok_or(RealmBrowserError::InexistentId)
   }
 
   pub fn for_each<F: FnMut(&RealmServer)>(&self, func: F) {
     let func = RefCell::new(func);
-    self.map.retain(|_, realm| {
+    self.realms.retain(|_, realm| {
       func.borrow_mut().deref_mut()(&realm);
       true
     });
@@ -122,10 +110,10 @@ impl RealmBrowser {
     &'a self,
     id: RealmServerId,
   ) -> Result<impl std::ops::Deref<Target = RealmServer> + 'a, RealmBrowserError> {
-    self.map.get(&id).ok_or(RealmBrowserError::InexistentId)
+    self.realms.get(&id).ok_or(RealmBrowserError::InexistentId)
   }
 
   pub fn len(&self) -> usize {
-    self.map.len()
+    self.realms.len()
   }
 }
